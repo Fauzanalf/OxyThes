d8c506a8b5ee31a79bc3f69a2efdd0a9e00167e0
Fauzanalf <89327323+Fauzanalf@users.noreply.github.com>
2022-05-17 06:19:23 +0700

    Update OxyConAP2.ino

--- OxyConAP2/OxyConAP2.ino
+++ OxyConAP2/OxyConAP2.ino
@@ -8,7 +8,13 @@
 
 ESP8266WebServer server(80);  //--> Server on port 80
 
-float DataReceive1, DataReceive2, DataReceive3;
+int DataReceive1;   //SpO2
+int DataReceive2;   //O2
+float DataReceive3; //LPM
+int DataReceive4;   //HR
+int DataReceive5;   //dummy
+
+//float DataReceive1, DataReceive2, DataReceive3, DataReceive4, DataReceive5; //SpO2, O2, LPM, HR, 1
 
 //----------------------------------------Make a wifi name and password as access point
 const char* ssid = "Oxythes";
@@ -25,10 +31,11 @@ void getDataString() {
       msg += Serial.readStringUntil('\n');
     }
 //    Serial.print(msg);
-    DataReceive1=splitString(msg,';',0).toFloat();
-    DataReceive2=splitString(msg,';',1).toFloat();
-    DataReceive3=splitString(msg,';',2).toInt();
-//    DataReceive4=splitString(msg,';',3);
+    DataReceive1=splitString(msg,';',0).toInt();
+    DataReceive2=splitString(msg,';',1).toInt();
+    DataReceive3=splitString(msg,';',2).toFloat();
+    DataReceive4=splitString(msg,';',3).toInt();
+    DataReceive5=splitString(msg,';',4).toInt();
   }
 }
 
@@ -38,23 +45,38 @@ void handleRoot() {
  server.send(200, "text/html", s); //--> Send web page
 }
 //----------------------------------------
-//----------------------------------------Procedure for reading the temperature value of a DHT11 sensor
+//----------------------------------------Procedure for reading the SpO2
 void handleOxthesSpO2() {
   getDataString();
-  //float t = dht.readTemperature();  //--> Read temperature as Celsius (the default). If you want to read the temperature sensor in Fahrenheit, use this -> float t = dht.readTemperature (true);
   String SpO2_Value = String(DataReceive1);
  
-  server.send(200, "text/plane", SpO2_Value); //--> Send Temperature value only to client ajax request
+  server.send(200, "text/plane", SpO2_Value); //--> Send SpO2 value only to client ajax request
   
 }
 //----------------------------------------
-//----------------------------------------Procedure for reading humidity values from DHT11 sensors
+//----------------------------------------Procedure for reading the O2
+void handleOxthesO2() {
+  getDataString();
+  String O2_Value = String(DataReceive2);
+ 
+  server.send(200, "text/plane", O2_Value); //--> Send O2 value only to client ajax request
+  
+}
+//----------------------------------------
+//----------------------------------------Procedure for reading LPM
 void handleOxythesLPM() {
   getDataString();
-  //float h = dht.readHumidity();
-  String LPM_Value = String(DataReceive2);
+  String LPM_Value = String(DataReceive3);
  
-  server.send(200, "text/plane", LPM_Value); //--> Send Humidity value only to client ajax request
+  server.send(200, "text/plane", LPM_Value); //--> Send LPM value only to client ajax request
+}
+//----------------------------------------
+//----------------------------------------Procedure for reading HR
+void handleOxythesHR() {
+  getDataString();
+  String HR_Value = String(DataReceive4);
+ 
+  server.send(200, "text/plane", HR_Value); //--> Send LPM value only to client ajax request
 }
 //----------------------------------------
 //----------------------------------------Setup
@@ -66,8 +88,10 @@ void setup(void){
 
   IPAddress apip = WiFi.softAPIP(); //--> Get the IP server
   server.on("/", handleRoot); //--> Routine to handle at root location. This is to display web page.
-  server.on("/readSpO2", handleOxthesSpO2);  //--> Routine to handle the call procedure handleDHT11Temperature
-  server.on("/readLPM", handleOxythesLPM);  //--> Routine to handle the call procedure handleDHT11Humidity
+  server.on("/readSpO2", handleOxthesSpO2);  //--> Routine to handle the call procedure
+  server.on("/readO2", handleOxthesO2);  //--> Routine to handle the call procedure
+  server.on("/readLPM", handleOxythesLPM);  //--> Routine to handle the call procedure
+  server.on("/readHR", handleOxythesHR);  //--> Routine to handle the call procedure
 
   server.begin(); //--> Start server
 }
